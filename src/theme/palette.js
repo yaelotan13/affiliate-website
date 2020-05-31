b1a8e614c1e9e1cee830584d03e07d9eef4cd032
yael.lotan <yaelotan19@gmail.com>
2020-05-31 10:17:59 +0300

    add-theme

---
+++ src/theme/palette.js
@@ -0,0 +1,13 @@
+const black = '#000000';
+
+export default {
+  black,
+  primary: "#D04460",
+  secondaryLightGrey: "#DADFD3",
+  textColor: "#4F4F4F",
+  darkGreen: "#4A4F40",
+  brightGreen: "#596F49",
+  lightGreen: "#D8DCD0",
+  superLightGreen: "#F2F4F1",
+  white: "#white",
+};
